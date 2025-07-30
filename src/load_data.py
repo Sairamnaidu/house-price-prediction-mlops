aa4d93c20cf3cf30559ab278ea5ee7dff5f57892
Sairam Naidu <sairamnaidu01@gmail.com>
2025-07-30 22:50:49 +0530

    Step -1 Adding  housing dataset and to track with DVC

---
+++ src/load_data.py
@@ -0,0 +1,14 @@
+from scikit-learn.datasets import fetch_california_housing
+import pandas as pd
+import os
+
+def load_and_save_data():
+    housing = fetch_california_housing(as_frame=True)
+    df = housing.frame
+
+    os.makedirs("data", exist_ok=True)
+    df.to_csv("data/housing.csv", index=False)
+    print("✅ Data saved to data/housing.csv")
+
+if __name__ == "__main__":
+    load_and_save_data()
